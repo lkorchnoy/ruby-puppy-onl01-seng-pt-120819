ed7c7663782b91f4491cf6ceeef821cd03b34321
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-13 21:14:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,4 +28,20 @@ end
   
 end
 
+def initialize(name)
+    @name = name
+    @@all << self
+    save
+  end
+
+  def self.all
+ @@ -20,4 +20,8 @@ def self.print_all
+  def self.clear_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+end
 
